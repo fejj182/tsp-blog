7f5ebb8be571eb789583190cf091721cabe028b8
Jeff Zinger <jzinger100@gmail.com>
2020-10-24 10:16:40 +0100

    Test publicPath to load nuxt payload.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -80,6 +80,7 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    publicPath: '/tspblog/_nuxt/',
     extend (config, { isDev }) {
       if (isDev) {
         return
