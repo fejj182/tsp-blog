2220cbe2ad61d1c885be2d9c25120fa3bd58db23
Jeff <jzinger100@gmail.com>
2020-12-29 14:39:40 +0000

    Change blog title

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,8 +8,8 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - trainspotter-blog',
-    title: 'trainspotter-blog',
+    titleTemplate: '%s - Sustainability Blog',
+    title: 'Sustainability Blog',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
